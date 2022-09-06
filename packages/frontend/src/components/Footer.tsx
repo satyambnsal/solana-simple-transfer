b248beeaabe3134f6d67fc5b2fd3f3069c6b5c66
Satyam Bansal <satyamsgsits1994@gmail.com>
2022-09-06 16:48:22 +0530

    Initial commit

---
+++ packages/frontend/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+
+export const Footer: FC = () => {
+    return (
+        <div className="relative bottom-0">
+            <footer className="mx-auto  flex flex-row p-2 text-center items-center footer bg-neutral text-neutral-content">
+            </footer>
+        </div>
+    );
+};
